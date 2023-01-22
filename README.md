4e5da70b23ac18395893852e092489dc378b6562
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-22 03:11:03 -0800

    make project:

---
+++ README.md
@@ -0,0 +1,18 @@
+### Product Filter
+
+---
+
+### Contact Me
+
+ * *Vanila JavaScript*
+ * *[Tailwind CSS Library](https://tailwindcss.com/)*
+ * *[Axios HTTP Client](https://axios-http.com/docs/intro/)*
+ * *[Fake Store API](https://fakestoreapi.com/)*
+
+---
+
+### Contact Me
+
+ * *[email](mailto:naderidefault@gmail.com)*
+ * *[Telegram](https://t.me/peymanath)*
+ * *[Linkedin](https://linkedin.com/in/peymanath)*
---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  content: [
+    '../src/**/*.html',
+    '../*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Vazirmatn'"],
+        serif: ["'Vazirmatn'"],
+        mono: ["'Vazirmatn'"],
+        display: ["'Vazirmatn'"],
+        body: ["'Vazirmatn'"],
+      },
+      extend: {
+        container: {
+          center: true,
+          padding: '1rem'
+        },
+      }
+    },
+  },
+  plugins: [],
+}
